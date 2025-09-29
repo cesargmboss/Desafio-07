dfe6151107d1a53ea8fa319ee66079ba484f183c
CAIO CÉSAR SANTOS PINTO <025.027492@universidadesenaicimatec.edu.br>
2025-09-29 16:13:12 -0300

    Link com API

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DashboardService } from '../services/dashboard.service';
 import { Veiculo } from '../models/veiculo.model';
-import { VehicleData } from '../models/vehicleData.model';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CarroVin } from '../utils/carroVinInterface';
@@ -15,13 +14,18 @@ import { MenuComponent } from "../menu/menu.component";
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   vehicles: Veiculo[] = [];
   selectedVehicle!: Veiculo;
-  vehicleData!: VehicleData;
-
   carVin!: CarroVin;
   reqVin!: Subscription;
+
+  private vehicleVinMap: { [key: number]: string } = {
+    1: "2FRHDUYS2Y63NHD22454",
+    2: "2RFAASDY54E4HDU34874",
+    3: "2FRHDUYS2Y63NHD22455",
+    4: "2RFAASDY54E4HDU34875",
+  };
 
   selectCarForms = new FormGroup({
     carId: new FormControl(''),
@@ -31,13 +35,25 @@ export class DashboardComponent implements OnInit {
     vin: new FormControl(''),
   });
 
-  onChange() {
+  loadVinDataOnInput() {
     this.vinForm.controls.vin.valueChanges.subscribe((value) => {
-      this.reqVin = this.dashboardservice
-        .buscarVin(value as string)
-        .subscribe((res) => {
-          this.carVin = res;
-        });
+      if (this.reqVin) {
+        this.reqVin.unsubscribe();
+      }
+     
+      const vinValue = value as string;
+
+      if (vinValue) {
+        this.reqVin = this.dashboardservice
+          .buscarVin(vinValue)
+          .subscribe((res) => {
+            this.carVin = res;
+          }, (error) => {
+              this.carVin = undefined as any;
+          });
+      } else {
+        this.carVin = undefined as any;
+      }
     });
   }
 
@@ -45,17 +61,44 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.dashboardservice.getVehicles().subscribe((res) => {
-      console.log(res.vehicles);
       this.vehicles = res.vehicles;
+
+      if (this.vehicles.length > 0) {
+        const defaultId = String(this.vehicles[0].id);
+        this.selectCarForms.controls.carId.setValue(defaultId, { emitEvent: true });
+      }
     });
+   
     this.selectCarForms.controls.carId.valueChanges.subscribe((id) => {
-      this.selectedVehicle = this.vehicles[Number(id) - 1];
+      const selectedId = Number(id);
+     
+      this.selectedVehicle = this.vehicles.find(v => Number(v.id) === selectedId) as Veiculo;
+     
+      const defaultVin = this.vehicleVinMap[selectedId];
+
+      if (defaultVin) {
+        this.vinForm.controls.vin.setValue(defaultVin, { emitEvent: false });
+        if (this.reqVin) {
+          this.reqVin.unsubscribe();
+        }
+       
+        this.reqVin = this.dashboardservice
+          .buscarVin(defaultVin)
+          .subscribe((res) => {
+            this.carVin = res;
+          });
+      } else {
+          this.vinForm.controls.vin.setValue('', { emitEvent: false });
+          this.carVin = undefined as any;
+      }
     });
-    this.onChange();
+   
+    this.loadVinDataOnInput();
   }
 
-  // ngOnDestroy(): void {
-  //   this.reqVin.unsubscribe();
-  // }
+  ngOnDestroy(): void {
+    if (this.reqVin) {
+      this.reqVin.unsubscribe();
+    }
   }
-
+}
